00d225a0e2bb6ef0d6b2d65f40f2061de93afcf8
Kyllian ROUSSELEAU <kyllianr1008@gmail.com>
2025-04-01 21:25:36 +0200

    second fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,8 @@ export default function Document() {
         <meta property="og:image:width" content="512" />
         <meta property="og:image:height" content="512" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="apple-itunes-app" content="app-id=no" />
+        <meta name="google-play-app" content="app-id=no" />
         
         {/* Désactiver le comportement PWA et empêcher l'invite d'installation */}
         <meta name="mobile-web-app-capable" content="no" />
